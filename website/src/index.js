b8293e551070e4f5d9cc03798ecefab217fcbd52
Felix Wieland <felix.wieland@mail.de>
2023-05-05 15:24:34 +0200

    website ideas

--- website/src/index.js
+++ website/src/index.js
@@ -10,59 +10,35 @@ function handleLightSwitch() {
     document.documentElement.classList.replace(setTo === 'light' ? 'dark' : 'light', setTo)
 }
 
-function handleNavbarSwitch() {
-    const nav = document.getElementsByTagName('nav')[0]
-    nav.style.display = nav.style.display === 'none' ? '' : 'none'
-}
-
-function handleEyecatcherSwitch() {
-    document.getElementById('eyecatcher').classList.toggle('full')
-}
-
 function startEyecatcherAnimations() {
-    // Some random colors
-    const colors = ["#2563eb", "#52525b", "#34d399", "#fb923c", "#db2777"];
-
-    const numBalls = 100;
-    const balls = [];
-
-    const target = document.getElementById("eyecatcher")
-
-    for (let i = 0; i < numBalls; i++) {
-        let ball = document.createElement("div");
-        ball.classList.add("point");
-        ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-        ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-        ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-        ball.style.transform = `scale(${Math.random()})`;
-        ball.style.width = `${Math.random()}em`;
-        ball.style.height = ball.style.width;
-
-        balls.push(ball);
-        target.append(ball);
+    const shape1 = document.getElementById("shape1")
+    const shape2 = document.getElementById("shape2")
+    const shape3 = document.getElementById("shape3")
+    window.onmousemove = function(e) {
+        shape1.animate({
+            left: `${e.clientX - 100}px`,
+            top: `${e.clientY}px`
+        }, {
+            duration: 6000,
+            fill: "forwards"
+        })
+        shape2.animate({
+            left: `${e.clientX + 200}px`,
+            top: `${e.clientY + 200}px`
+        }, {
+            duration: 4000,
+            fill: "forwards"
+        })
+        shape3.animate({
+            left: `${e.clientX}px`,
+            top: `${e.clientY - 200}px`
+        }, {
+            duration: 8000,
+            fill: "forwards"
+        })
     }
-
-    // Keyframes
-    balls.forEach((el, i, ra) => {
-        let to = {
-            x: Math.random() * (i % 2 === 0 ? -11 : 11),
-            y: Math.random() * 12
-        };
-
-        let anim = el.animate(
-            [
-                { transform: "translate(0, 0)" },
-                { transform: `translate(${to.x}rem, ${to.y}rem)` }
-            ], {
-                duration: (Math.random() + 1) * 500, // random duration
-                direction: "alternate",
-                fill: "both",
-                iterations: Infinity,
-                easing: "ease-in-out"
-            }
-        );
-    });
 }
+
 
 function lightSwitchEventListener() {
     Array.from(document.getElementsByClassName("lightswitch")).forEach(function(e) {
@@ -79,14 +55,6 @@ function addShortcutEventListener() {
             handleLightSwitch()
             handled = true
         }
-        if ((e.metaKey || e.ctrlKey) && e.key === 'b') {
-            handleNavbarSwitch()
-            handled = true
-        }
-        if ((e.metaKey || e.ctrlKey) && e.key === 'j') {
-            handleEyecatcherSwitch()
-            handled = true
-        }
         if (handled) {
             if (e && e.stopPropagation) {
                 e.stopPropagation();
@@ -99,7 +67,7 @@ function addShortcutEventListener() {
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    addShortcutEventListener()
     lightSwitchEventListener()
+    addShortcutEventListener()
     startEyecatcherAnimations()
 });
