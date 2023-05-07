d6fcb325827646d8b1bec5fe704f8eca52da99d2
Felix Wieland <felix.wieland@mail.de>
2023-05-07 20:38:22 +0200

    continued with website

--- website/src/index.js
+++ website/src/index.js
@@ -1,42 +1,90 @@
 /* @refresh reload */
-import './../index.css'
+import './../assets/css/index.css'
 
-const twlightmode = 'tw-lightmode'
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+};
 
-function handleLightSwitch() {
-    const isLight = document.documentElement.classList.contains('light')
-    const setTo = isLight ? 'dark' : 'light'
-    localStorage.setItem(twlightmode, setTo)
-    document.documentElement.classList.replace(setTo === 'light' ? 'dark' : 'light', setTo)
+const texts = [
+    "JavaScript",
+    "TypeScript",
+    "GraphQL",
+    "REST",
+    "Bundle sizes",
+    "NPM",
+    "Build times",
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
 }
 
-function lightSwitchEventListener() {
-    Array.from(document.getElementsByClassName("lightswitch")).forEach(function(e) {
-        e.addEventListener("click", function(event) {
-            handleLightSwitch()
-        })
-    })
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
 }
 
-function addShortcutEventListener() {
-    window.addEventListener('keydown', (e) => {
-        let handled = false
-        if ((e.metaKey || e.ctrlKey) && e.key === 'l') {
-            handleLightSwitch()
-            handled = true
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+    cooldown -= dt;
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
         }
-        if (handled) {
-            if (e && e.stopPropagation) {
-                e.stopPropagation();
-            } else if (e && window.event) {
-                window.event.cancelBubble = true;
-            }
-            e.preventDefault();
-        }
-    })
+        doMorph();
+    } else {
+        doCooldown();
+    }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    lightSwitchEventListener()
-    addShortcutEventListener()
-});
+document.getElementById("open-menu").addEventListener("click", () => {
+    document.getElementById("menu").style.display = "block"
+})
+document.getElementById("close-menu").addEventListener("click", () => {
+    document.getElementById("menu").style.display = "none"
+})
+
+animate();
